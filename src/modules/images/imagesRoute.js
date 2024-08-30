8dbb59d44a320db9141d6f1f81ff427da722b7e4
rosselgalarzarafael <rosselgalarzarafael@gmail.com>
2024-08-30 16:50:06 -0500

    eliminación d eun bug en la ruta dfe eliminar imagen de producto

--- src/modules/images/imagesRoute.js
+++ src/modules/images/imagesRoute.js
@@ -4,6 +4,6 @@ const imagesController = require('./imagesController');
 
 // Ruta para obtener una imagen
 router.get('/:imageName', imagesController.sendImage);
-router.delete('products/:productId', imagesController.deleteImagesController);
+router.delete('/products/:productId', imagesController.deleteImagesController);
 router.get('/list/:productId', imagesController.listImagesController);
 module.exports = router;
